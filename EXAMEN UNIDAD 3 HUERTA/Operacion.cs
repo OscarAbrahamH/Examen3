4b02e84b54b209f121ec6a94f8aa87540611fecb
OscarAbrahamH <43561380+OscarAbrahamH@users.noreply.github.com>
2018-10-31 16:31:08 -0700

    Add files via upload

---
+++ EXAMEN UNIDAD 3 HUERTA/Operacion.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace E3_EstDatos
+{
+    public class Operacion
+    {
+        public void Principal()
+        {
+            Stack Lista = new Stack();
+            Queue Cola = new Queue();
+
+        }
+
+        public void Ejercicio1()
+        {
+            //¿Qué valores se devuelven durante la siguiente serie de operaciones de pila,
+            //si se ejecutan en una pila inicialmente vacía ?
+            //push(5), push(3), pop(), push(2), push(8),
+            //pop(), pop(), push(9), push(1), pop(), push(7), push(6), pop(), pop(), push(4),
+            //pop(), pop()
+        }
+
+        public void Ejercicio2()
+        {
+            //Escriba en este metodo un modulo que pueda leer  y almacenar palabras reservadas en una lista enlazada 
+            //e identificadores y literales en Otra lista enlazada.
+            //Cuando el programa haya terminado de leer la entrada, mostrar
+            //Los contenidos de cada lista enlazada.
+            //Revise que es un Identificador y que es un literal
+        }
+
+        public void Ejercicio3()
+        {
+            //mida el tiempo entre Un lista ligada y una lista normal en el tiempo de ejecucion de 9876 elementos agregados.
+        }
+
+        public void Ejercicio4()
+        {
+            //Diseñar e implementar una clase que le permita a un maestro hacer un seguimiento de las calificaciones
+            // en un solo curso.Incluir métodos que calculen la nota media, la
+            //calificacion más alto, y el calificacion más bajo. Escribe un programa para poner a prueba tu clase.
+            //implementación. Minimo 30 Calificaciones, de 30 alumnos.
+        }
+    }
+}
---
+++ EXAMEN UNIDAD 3 HUERTA/Program.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace E3_EstDatos
+{
+    class Program
+    {
+        public static int c;
+        public static string[,] act2 = new string[100, 2];
+        static Stack<int> GetStack()
+        {
+            Stack<int> stack = new Stack<int>();
+            stack.Push(5);
+            stack.Push(3);
+            stack.Pop();
+            stack.Push(2);
+            stack.Push(8);
+            stack.Pop();
+            stack.Pop();
+            stack.Push(9);
+            stack.Push(1);
+            stack.Pop();
+            stack.Push(7);
+            stack.Push(6);
+            stack.Pop();
+            stack.Pop();
+            stack.Push(4);
+            stack.Pop();
+            stack.Pop();
+            return stack;
+        }
+        static void Main(string[] args)
+        {
+            do
+            {
+                Console.WriteLine("Seleccione ejercicio a ejecutar:");
+                Console.WriteLine("0) Salir");
+                Console.WriteLine("1) Actividad 1");
+                Console.WriteLine("2) Actividad 2");
+
+                string r = Console.ReadLine();
+                c = Int32.Parse(r);
+                if (c == 1){
+                        var stack = GetStack();
+                        Console.WriteLine("--- Stack contents ---");
+                        foreach (int i in stack)
+                        {
+                            Console.WriteLine(i);
+                        }
+                        Console.ReadKey();
+                }
+                else if (c==2){
+                        Console.WriteLine("Instrucciones:");
+                        Console.WriteLine("1) Registrar");
+                        Console.WriteLine("2) Mostrar datos");
+                        string a = Console.ReadLine();
+                        int b = Int32.Parse(a);
+                        if (b==1)
+                        {
+                                Console.WriteLine("Declare:");
+                                bool check = false;
+                                string d = Console.ReadLine();
+                                string[] words = d.Split(' ');
+                                for (int i = 0; i <= 99; i++)
+                                {
+                                        for (int j = 0; j <= 1; j++)
+                                        {
+                                            if (act2[i, j] == null)
+                                            {
+                                                act2[i, j] = words[j];
+                                                check = true;
+                                            }
+                                            
+                                        }
+                                        if (check == true)
+                                        {
+                                            i = 100;
+                                        }
+                                }
+                        }else if(b==2)
+                        {
+                                for (int i = 0; i <= 99; i++)
+                                {
+
+                                    for (int j = 0; j <= 1; j++)
+                                    {
+                                        if (act2[i, j] != null)
+                                        {
+                                            if (j == 1)
+                                            {
+                                                Console.WriteLine(act2[i, j]);
+                                            }
+                                            else
+                                            {
+                                                Console.Write(" "+act2[i, j]);
+                                            }
+                                        }
+                                    }
+
+
+
+                                }
+                        }
+
+                }
+                else if (c == 3)
+                {
+                    Clase mate = new Clase();
+
+                }
+                else if (c != 1 && c != 2 && c != 0)
+                {
+                    Console.WriteLine("Seleccione un numero valido");
+                }
+
+            } while (c != 0);
+        }
+            
+            
+    }
+}
